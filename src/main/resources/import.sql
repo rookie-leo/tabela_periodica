b30f73c15303536bd0e6117470a756f76cf6e3e0
rookie-leo <proleonardopa@gmail.com>
2023-04-27 15:08:20 -0300

    [fix] Troca do sgdb para POSTGRESQL. Implementado cadastro de novo elemento

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
-insert into Elemento (id, numeroAtomico, nome, simbolo, massaAtomica) values(1, 89, 'Actínio', 'Ac', '227');
-insert into Elemento (id, numeroAtomico, nome, simbolo, massaAtomica) values(2, 35, 'Bromo', 'Br', '79.909');
-insert into Elemento (id, numeroAtomico, nome, simbolo, massaAtomica) values(3, 6, 'Carbono', 'C', '12.01115');
+insert into Elemento (id, numeroAtomico, nome, simbolo, massaAtomica) values(nextval('hibernate_sequence'), 89, 'Actínio', 'Ac', '227');
+insert into Elemento (id, numeroAtomico, nome, simbolo, massaAtomica) values(nextval('hibernate_sequence'), 35, 'Bromo', 'Br', '79.909');
+insert into Elemento (id, numeroAtomico, nome, simbolo, massaAtomica) values(nextval('hibernate_sequence'), 6, 'Carbono', 'C', '12.01115');
